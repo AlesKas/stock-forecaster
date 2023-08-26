2fa21367394037105bc34be036f87c5c2ac7a82e
AlesKas <ales.kasparek@email.cz>
2023-08-26 12:52:38 +0200

    Switch api provider

--- src/routes/stocks.py
+++ src/routes/stocks.py
@@ -2,7 +2,6 @@ import os
 import requests
 
 from fastapi import APIRouter
-from utils.date import date, daterange
 from requests.auth import HTTPBasicAuth
 from fastapi.responses import JSONResponse
 from db_handler.influxHandler import InfluxHandler
@@ -26,17 +25,10 @@ async def post_stock_ticket(ticket : str):
 @router.post('/stock/')
 async def post_stock():
     headers = {
-        'Accept': 'application/json',
-        'Authorization' : f"Bearer {API_KEY}"
+        "X-RapidAPI-Key": API_KEY,
+        "X-RapidAPI-Host": "yahoo-finance15.p.rapidapi.com"
     }
 
-    start_date = date(2022, 1, 3)
-    end_date = date(2021, 2, 1)
-    # for single_date in daterange(start_date, end_date):
-    #     print(single_date.strftime("%Y-%m-%d"))
-    url = API_BASE_URL + f'aggs/grouped/locale/us/market/stocks/{start_date.strftime("%Y-%m-%d")}'
+    url = API_BASE_URL + f'hi/history/AAPL/1d'
     print(url)
     r = requests.get(url, headers=headers)
-    print(r.status_code)
-    print(r.content)
-    print(r.json())
